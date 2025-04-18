d8ac6286966e8e8a2a9bdd084b3ae75a0aeaabd5
QuangHaiz115 <haibuidg@gmail.com>
2025-04-19 03:27:03 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app.routes.user_routes import user_bp
+from app.routes.order_routes import order_bp
+from app.routes.food_routes import food_bp
+from app.routes.drink_routes import drink_bp
 
 app = Flask(__name__)
 app.register_blueprint(user_bp, url_prefix='/user')
+app.register_blueprint(order_bp, url_prefix='/orders')
+app.register_blueprint(food_bp, url_prefix='/food')
+app.register_blueprint(drink_bp, url_prefix='/drink')
 
 if __name__ == '__main__':
     app.run(debug=True)
